7732b768825e4d9d8a4e456f90b3a7cbf4109d85
Wang lei <wang.lei@gmail.com>
2012-12-07 11:43:20 +0800

    provide setenv.sh to export the values for different host and database

--- groovy/email-alert/setenv.sh
+++ groovy/email-alert/setenv.sh
@@ -1,16 +1,1 @@
-export USER_EMAIL=wang.lei@rea-group.com
-
-export MYREA_HOST=172.20.56.242
-export MYREA_DB=rea
-export MYREA_USER=root
-export MYREA_PASS=s3cr3t
-
-export PSEEKER_HOST=172.20.46.193
-export PSEEKER_DB=JetwireTest
-export PSEEKER_USER=casa
-export PSEEKER_PASS=casa
-
-export EMAIL_ALERT_HOST=172.20.49.18
-export EMAIL_ALERT_DB=email_alert
-export EMAIL_ALERT_USER=root
-export EMAIL_ALERT_PASS=s3cr3t
+setenv.sh.local
