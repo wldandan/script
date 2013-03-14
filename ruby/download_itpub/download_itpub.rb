98da24f96b4a1b011b44066bc00dd07c26298717
Wang lei <wang.lei@gmail.com>
2013-03-14 13:05:08 +0800

    remove duplicate code

--- ruby/download_itpub/download_itpub.rb
+++ ruby/download_itpub/download_itpub.rb
@@ -5,12 +5,12 @@ require 'json'
 baseurl=ARGV[0]
 puts baseurl
 doc=Nokogiri::HTML(open(baseurl))
-
-links = doc.css("ignore_js_op span a").each do |item|
+parse_rule = "ignore_js_op span a"
+#parse_rule = ignore_js_op a
+links = doc.css(parse_rule).each do |item|
   relative_url = item['href']
   title=item.inner_text
   url="http://www.itpub.net/#{relative_url}"
   url.gsub!('attachment.php?','forum.php?mod=attachment&')
-  #system "curl -o '#{title}' '#{url}'"
   system "curl -o \"#{title}\" '#{url}'"
 end  
